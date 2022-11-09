fdce040966a4f8afc74bcdd8780e74b9aa9e9dee
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2022-11-09 13:31:35 -0500

    ConsoleDriver comments

---
+++ src/ConsoleDriver.java
@@ -0,0 +1,16 @@
+public class ConsoleDriver {
+    public static void main(String[] args) {
+        // bulk of logic is elsewhere
+        // this should only have IO logic
+        // meaning talk to user, get user input, show changes to user
+
+        // eg
+        // create on object type Game, Adventure...
+        // Game g = new Game();
+        // maybe while(!g.hasWon()) {}
+        // ask user for input
+        // call g.displayStatus() --- or displayBoard()
+
+        // can add GUI with minimal if any changes to game logic
+    }
+}
