f7e2a060df75100045bb659adfe991ae4dd618c6
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2022-11-09 13:16:04 -0500

    Neatened "enter name" prompt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter your name");
+        System.out.println("Please enter your name: ");
         java.lang.String name = in.nextLine();
 
 
