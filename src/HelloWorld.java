815fde38725f11b454570547321de32e7d7d6307
MUNCYMM20 <Muncymm20@gcc.edu>
2022-11-08 21:38:25 -0500

    Edited to ask user for name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static <String> void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        java.lang.String name = in.nextLine();
+
+
+        System.out.println("Hello " + name);
     }
 }
